59bce4de8307fcc5a0f932da4d7b2116661994c5
RedTechX0 <jeremy.fangyc@gmail.com>
2024-01-13 15:03:55 +0800

    im don
    (:

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ Q1:
 
 # TODO: Write your code here
 
+for i in range(100000):
+    print(i)
 ##################################################
 '''
 Q2:
@@ -12,6 +14,12 @@ Q2:
 
 # TODO: Write your code here
 
+sum = 0
+
+for i in range(100000):
+    sum += i
+
+print(sum)
 ##################################################
 '''
 Q3:
@@ -19,6 +27,8 @@ Q3:
 
 # TODO: Write your code here
 
+for i in range(2000):
+    print(i + 1 * 2)
 ##################################################
 '''
 Q4:
@@ -26,6 +36,8 @@ Q4:
 
 # TODO: Write your code here
 
+for i in range(1, 2000):
+    print(100 - i * 3)
 ##################################################
 '''
 Q5:
@@ -33,6 +45,17 @@ Q5:
 
 # TODO: Write your code here
 
+# a): 
+for i in range(1, 100000):
+    print(i ** 2)
+
+# b): 
+n = 0
+step = 1
+for i in range(10):
+    n += step
+    step += 2
+    print(n)
 ##################################################
 '''
 Q6:
@@ -40,4 +63,44 @@ Q6:
 
 # TODO: Write your code here
 
+#There are 2 ways:
+
+for i in range(4):
+    print('*' * 5)
+
+#or,
+
+    print('*****\n' * 5)
 ##################################################
+'''
+Q7:
+'''
+
+# TODO: Write your code here
+
+for i in range(4):
+    print('*' * (i + 1))
+##################################################
+'''
+Q8:
+'''
+
+# TODO: Write your code here
+
+for i in range(5):
+    print('*' * (6 - i))
+##################################################
+'''
+Q9:
+'''
+
+# TODO: Write your code here
+
+for i in range(5):
+    print(f'{i}' * i)
+##################################################
+
+#Give a review:
+#    stars: *****
+#    did you learn something?    (Y)   N     (braket one like this: (~))
+#    comment (optional): 
